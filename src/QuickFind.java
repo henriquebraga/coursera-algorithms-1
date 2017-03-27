f9a6d3a58614680e929edda15ecdc5d2ab35d114
Henrique Braga <henrique.inftec@hotmail.com>
2017-03-26 23:33:05 -0300

    implements QuickFind search

---
+++ src/QuickFind.java
@@ -0,0 +1,58 @@
+
+public class QuickFind {
+	/*
+	 * Quick Find: Eager approach
+	 * Data Strucutre.
+	 * Integer array id[] of size n
+	 * Interpretation: p and q are connected if they have the same id.
+	 * 
+	 * -> Find. Check if p and q have the same id
+	 * 
+	 * -> Union. To merge components containing p and q, change all entries 
+	 * whose id equals id[p] to o id[q]
+	 * 
+	 * 
+	 * Example:
+	 * 
+	 * id [] 
+	 * Sets: {0, 5, 6}, {1, 2, 7}, {3, 4, 8, 9}
+	 * 
+	 * pos  id 
+	 * 0    0
+	 * 1    1
+	 * 2    1
+	 * 3    8
+	 * 4    8
+	 * 5    0
+	 * 6    0
+	 * 7    1
+	 * 8    8
+	 * 9    8
+	 */
+	int id[];
+	
+	public QuickFind(int size){
+		id = new int[size];	
+		
+		for(int i = 0; i < id.length; i++){
+			id[i] = i;
+		}
+	}
+
+	public boolean areConnected(int p, int q) {
+		return id[p] == id[q];
+	}
+
+	public void union(int p, int q) {
+		int qId = this.id[q];
+		
+		for (int i = 0; i < this.id.length; i++){			
+			if(this.id[i] == qId){
+				this.id[i] = this.id[p];
+				
+			}
+			
+		}
+	}
+
+}
---
+++ src/TestQuickFind.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+public class TestQuickFind extends TestCase {
+	
+	private QuickFind qf;
+
+	private int p;
+	private int q;
+	
+	protected void setUp() {
+		this.p = 3;
+		this.q = 4;
+		this.qf = new QuickFind(10);
+	}
+	@Test
+	public void testShouldCreateTenSizeArray() {
+		assertEquals(this.qf.id.length, 10);	
+	}
+	
+	@Test
+	public void testWhenCreatesPositionsShouldHaveTheSameValueFromIndex(){
+		int expectedIds[] = new int[10];
+		
+		for (int i = 0; i < expectedIds.length; i++){
+			expectedIds[i] = i;
+		}
+		
+		assertArrayEquals(expectedIds, this.qf.id);
+	}
+	
+	@Test
+	public void testPandQshouldNotBeConnected(){
+		assertFalse(this.qf.areConnected(this.p, q));
+	}
+	
+	@Test
+	public void testAfterUnionComponentsShouldHaveSameIds(){
+		this.qf.union(p, q);
+		assertEquals(this.qf.id[p], this.qf.id[q]);
+	}
+	
+	@Test
+	public void testShouldUnionTwoComponents(){
+		// 0 1 2 3 4 5 6 7 8 9 initial state
+		// 0 1 2 3 3 5 6 7 8 9 union(3, 4)
+		// 0 0 2 3 3 5 6 7 8 9 union(0, 1)
+		// 0 0 2 0 0 5 6 7 8 9 union(1, 3): should union elements containing id 3
+		
+		this.qf.union(3, 4);
+		this.qf.union(0, 1);
+		this.qf.union(1, 3);
+		
+		int[] expectedIds = {0, 0, 2, 0, 0, 5, 6, 7, 8, 9};
+		
+		assertArrayEquals(expectedIds, this.qf.id);		
+	}
+
+}
