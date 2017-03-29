30d16058299034d40a3a796ba9219ba59feb90bb
Henrique Braga <henrique.inftec@hotmail.com>
2017-03-28 22:56:37 -0300

    pulls up constructor to DynamicConectivity class

--- src/QuickFind/DynamicConectivity.java
+++ src/QuickFind/DynamicConectivity.java
@@ -7,4 +7,11 @@ public abstract class DynamicConectivity {
 	public abstract boolean areConnected(int p, int q);
 	public abstract void union(int p, int q);
 	
+	public DynamicConectivity(int size){
+		id = new int[size];	
+		for(int i = 0; i < id.length; i++){
+			id[i] = i;
+		}
+	}
+	
 }
---
+++ src/QuickFind/QuickUnion.java
@@ -0,0 +1,23 @@
+package QuickFind;
+
+public class QuickUnion extends DynamicConectivity {
+
+	public QuickUnion(int i) {
+		super(i);
+		this.id = new int[i];
+		
+	}
+
+	@Override
+	public boolean areConnected(int p, int q) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void union(int p, int q) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
